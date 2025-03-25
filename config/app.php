5698bed8543ff6a2e0c07ab7c5c69b741f43e282
jayben <jay.ayamee@gmail.com>
2025-03-25 10:48:08 +0100

    add domain to config.app

--- config/app.php
+++ config/app.php
@@ -3,6 +3,20 @@
 return [
 
     /*
+   |--------------------------------------------------------------------------
+   | Application Domain
+   |--------------------------------------------------------------------------
+   |
+   | This value is the name of your application, which will be used when the
+   | framework needs to place the application's name in a notification or
+   | other UI elements where an application name needs to be displayed.
+   |
+   */
+
+    'domain' => env('APP_DOMAIN', 'michaelzingraf.com'),
+
+
+    /*
     |--------------------------------------------------------------------------
     | Application Name
     |--------------------------------------------------------------------------
